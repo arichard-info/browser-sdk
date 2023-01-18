43fdbb861bce7101bc9f9cc6c4c4fc9bbd144626
Benoît Zugmeyer <benoit.zugmeyer@datadoghq.com>
2023-01-18 15:01:24 +0100

    ⚗🐛 [RUMF-1296] use pointerup to trigger click actions
    This slightly change the action beginning, but takes any activity
    produced by 'pointerup' 'mouseup' 'touchend' events into account. If
    those actions don't produce any activity, the action start is likely to
    be the same as before, since 'pointerup/mouseup/touchend' should be
    triggered very close to the 'click' event.

--- packages/rum-core/src/domain/rumEventsCollection/action/trackClickActions.spec.ts
+++ packages/rum-core/src/domain/rumEventsCollection/action/trackClickActions.spec.ts
@@ -429,6 +429,26 @@ describe('trackClickActions', () => {
         expect(events.length).toBe(1)
         expect(events[0].frustrationTypes).toEqual([FrustrationType.DEAD_CLICK])
       })
+
+      describe('click_action_on_pointerup experimental feature', () => {
+        beforeEach(() => {
+          updateExperimentalFeatures(['click_action_on_pointerup'])
+        })
+
+        afterEach(() => {
+          resetExperimentalFeatures()
+        })
+
+        it('does not consider a click with activity happening on pointerup as a dead click', () => {
+          const { clock } = setupBuilder.build()
+
+          emulateClick({ activity: { on: 'pointerup' } })
+
+          clock.tick(EXPIRE_DELAY)
+          expect(events.length).toBe(1)
+          expect(events[0].frustrationTypes).toEqual([])
+        })
+      })
     })
   })
 
@@ -439,6 +459,7 @@ describe('trackClickActions', () => {
     target?: HTMLElement
     activity?: {
       delay?: number
+      on?: 'pointerup' | 'click'
     }
   } = {}) {
     const targetPosition = target.getBoundingClientRect()
@@ -456,12 +477,16 @@ describe('trackClickActions', () => {
     target.dispatchEvent(createNewEvent('pointerdown', eventProperties))
     setupBuilder.clock!.tick(EMULATED_CLICK_DURATION)
     target.dispatchEvent(createNewEvent('pointerup', eventProperties))
+    emulateActivityIfNeeded('pointerup')
     target.dispatchEvent(createNewEvent('click', eventProperties))
+    emulateActivityIfNeeded('click')
 
-    if (activity) {
-      setupBuilder.clock!.tick(activity.delay ?? BEFORE_PAGE_ACTIVITY_VALIDATION_DELAY)
-      // Since we don't collect dom mutations for this test, manually dispatch one
-      setupBuilder.domMutationObservable.notify()
+    function emulateActivityIfNeeded(event: 'pointerup' | 'click') {
+      if (activity && (activity.on ?? 'click') === event) {
+        setupBuilder.clock!.tick(activity.delay ?? BEFORE_PAGE_ACTIVITY_VALIDATION_DELAY)
+        // Since we don't collect dom mutations for this test, manually dispatch one
+        setupBuilder.domMutationObservable.notify()
+      }
     }
   }
 
